550a985f516bae34196658123853a2bac628d961
shm0nya <shm0nya@yandex.ru>
2016-04-09 01:39:47 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,17 +7,17 @@
 
 using namespace std;
 
-#include "Sfiles.h"
+#include "Sfiles.h"// создано для ввода-вывода из файла
 
-map <int, int> swap(map <int, int> tabl, int count);
-map <int, int> generate_tabl(int count);
-vector <int> generate_key(int n, int m, int j);
-vector < vector <int> > sub(vector<int> key, int n, int m, int j);
-int Counter_Bits(vector <string> &text); // Подсчет битов в тексте
-string Random_Bytes(int m); // Генерация случайной последовательности 0 и 1 длины m
-vector <string> Random_sbox(int m); // Генерация случайного s-block (нужно для подстановки)
-map <string, string> generate_tabl_mix(map <string, string> sblock, int m); // Генерация таблицы подстановки
-map <string, string> generate_sbox(int m); // Генерация s-block
+map <int, int> generate_tabl(int count);//Cоздает таблицу замены размер n*m - 2. Используем во время xor с ключом. Таблица еще не перемешана, мешаем в swap
+map <int, int> swap(map <int, int> tabl, int count);//Перемешивает таблицу замены.
+vector <int> generate_key(int n, int m, int j);//Cоздает ключ размера n*m*j. Ключ еще не разбит на K(i)
+vector < vector <int> > sub(vector<int> key, int n, int m, int j); //Разбиваем ключ на K(i)
+int Counter_Bits(vector <string> &text); //Подсчет битов в тексте
+string Random_Bytes(int m); //Генерация случайной последовательности 0 и 1 длины m
+map <string, string> generate_sbox(int m); //Создает таблицу размера (2^m - 2). В ней как по таблице истинность упорядочены 000, 001... (смотри дальше Random_sbox)
+vector <string> Random_sbox(int m); //Генерация s-блока, причем, мы их перемешиваем, после запишем в map и полчим sbox (смотри generate_tabl_mix )
+map <string, string> generate_tabl_mix(map <string, string> sblock, int m); // Генерация таблицы подстановки sbox
 
 int main()
 {
@@ -160,3 +160,4 @@ map <string, string> generate_tabl_mix (map <string, string> sblock, int m)
 	}
 	return sblock;
 }
+
