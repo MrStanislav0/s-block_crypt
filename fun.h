634c163df491a7fa3f9f67ab7953b8a30541304e
shm0nya <shm0nya@yandex.ru>
2016-04-10 01:18:21 +0300

    вернул swap
    Косметические изменения, вернул swap

--- fun.h
+++ fun.h
@@ -1,14 +1,18 @@
 #ifndef MODULE3_H
 #define MODULE3_H
 
-map <int, int> generate_tabl(int count);//Cоздает таблицу замены размер n*m - 2. Используем во время xor с ключом. Таблица еще не перемешана, мешаем в swap
+map <int, int> generate_tabl(int n, int m);//Cоздает таблицу замены размер n*m - 2. Используем во время xor с ключом. Таблица еще не перемешана, мешаем в swap
+map <int, int> swap(map <int, int> tabl, int n,int m);//Перемешивает таблицу замены. Получим p-блок
+
 vector <int> generate_key(int n, int m, int j);//Cоздает ключ размера n*m*j. Ключ еще не разбит на K(i)
 vector < vector <int> > sub(vector<int> key, int n, int m, int j); //Разбиваем ключ на K(i)
 int Counter_Bits(vector <string> &text); //Подсчет битов в тексте
-string Random_Bits(int m); //Генерация случайной последовательности 0 и 1 длины m
+string Random_Bits(int m); //Генерация случайной последовательности 0 и 1 длины m, то есть текста
+
 map <string, string> generate_sbox(int m); //Создает таблицу размера (2^m - 2). В ней как по таблице истинность упорядочены 000, 001... (смотри дальше Random_sbox)
 vector <string> Random_sbox(int m); //Генерация s-блока, причем, мы их перемешиваем, после запишем в map и полчим sbox (смотри generate_tabl_mix )
-map <string, string> generate_tabl_mix(map <string, string> sblock, int m); // Генерация таблицы подстановки sbox
+map <string, string> generate_tabl_mix(map <string, string> sblock, int m); // Генерация таблицы подстановки s-блок
+
 vector <string> sub_str_blok (string, int);//Разбивает текст на кучу блоков для дальнейшей замены (покачто хрен знает сколько блоков, размера m, ибо генерируем текст n*m - переполнения не будет)
 
 #endif 
