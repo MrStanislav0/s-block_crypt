8ef7f458ab929171d36021300b1d812c92564c71
Stanislav <graffiti007@yandex.ru>
2016-04-10 20:02:48 +0300

    Переписаны комментарии [1]
    В main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,6 @@
 
 using namespace std;
 
-#include "Sfiles.h"// создано для ввода-вывода из файла
-#include "fun.h"
-
 /*
 map <int, int> generate_tabl(int n, int m);//Cоздает таблицу замены размер n*m - 2. Используем во время xor с ключом. Таблица еще не перемешана, мешаем в swap
 map <int, int> swap(map <int, int> tabl, int n, int m);//Перемешивает таблицу замены.
@@ -29,35 +26,38 @@ vector <vector <string>> use_s_box (vector <vector <string>> hs, map <string,str
 string XOR (vector <vector <string>> hs, map<int,int> p_box, vector< vector<int>> sub_key, int j);//собственно последние два блока, как я понял. XOR, согласно p_box
 */
 
+#include "Sfiles.h"// Работа с файлами
+#include "fun.h" // Все остальные функции
+
 int main()
 {
-	srand(time(0));// делаем, дабы был ГСЧ нормальным, а не как обычно (начальное значение случайное)
+	srand(time(0));
 
-	int n,m,j; //Кол-во блоков, размер блока, количество раундов
-	vector <int> key;//Ключ
-	vector <vector <int>> sub_key;//Разбитый ключ
-	map <string,string> sbox;//С-блоки
-	map<int,int> p_box;//таблица замены, она же функция подстановки (перед XOR)
-	string text;
-	vector <vector <string>> hs;//тут разбитый на (хз сколько) по n блоков текст лежит
-	string new_text;//текст после xor
+	int n, m, j; // Кол-во блоков, размер блока, количество раундов
+	vector <int> key; // Ключ
+	vector <vector <int>> sub_key; // Разбитый ключ
+	map <string,string> sbox; // s-блок
+	map<int,int> p_box; // таблица замены, она же функция подстановки (перед XOR)
+	string text; // Исходный текст
+	vector <vector <string>> hs; // Разбитый на части исходный текст
+	string new_text; // Зашифрованный текст
 	
-	cin>>n>>m>>j;
+	cin >> n >> m >> j;
 	
-	key=generate_key(n, m, j);
-	sub_key=sub(key,n,m,j);
+	key = generate_key(n, m, j); // Генерация ключа
+	sub_key = sub(key,n,m,j); // Разбиение ключа
 	
 	
-	sbox=generate_sbox(m);
-	sbox=generate_tabl_mix(sbox,m);
+	sbox = generate_sbox(m); // Генерация s-блока
+	sbox = generate_tabl_mix(sbox,m); // Перемешивание элементов в таблице
 
-	p_box=generate_tabl(n,m);
-	p_box=swap(p_box,n,m);
+	p_box = generate_tabl(n,m); // Генерация p-блока
+	p_box = swap(p_box,n,m); // Перемешивание элементов в таблице
 
-	text=generate_text (n,m);
-	hs=sub_str_blok(text,n,m);//разбили, 
-	hs=use_s_box(hs,sbox);//заменили.
-	new_text=XOR(hs,p_box, sub_key,j);//Сложили по модулю 2 с ключом
+	text = generate_text (n,m); // Генерация текста (тестовая функция)
+	hs = sub_str_blok(text,n,m); // Разбиение текста 
+	hs = use_s_box(hs,sbox); // Замена битов в тексте в соотвествии с таблицей замены
+	new_text = XOR(hs,p_box, sub_key,j); // Сложение по модулю 2 текста и ключа в соотвествии с p-блоком
 
 	return 0;
 }
